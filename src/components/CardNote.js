3053f60d768924414d1ee8490d55ff3c86ea762f
Mary <marytian.mt@gmail.com>
2021-12-23 13:58:48 -0700

    Wrote a function to increase likes.

--- src/components/CardNote.js
+++ src/components/CardNote.js
@@ -1,8 +1,22 @@
 import React from "react";
 import "./CardNote.css";
+import { useState } from 'react';
 import PropTypes from "prop-types";
 
 const CardNote = () => {
+
+  const [likesCount, setLikesCount] = useState(0);
+  const increaseLikes = () => {
+      setLikesCount(likesCount + 1);
+  };
+
+  // return (
+  //   <section>
+  //     <p>The number of likes is {likesCount}.</p>
+  //     <button onClick={increaseLikes}>Like</button>
+  //   </section>
+  // );
+
   return (
     <section className="card-note card-container">
       <div className="card-para">
@@ -13,12 +27,12 @@ const CardNote = () => {
       </div>
       <div className="card-footer">
         <div className="heart">
-          <span>0</span>
+          <span>{likesCount}</span>
           <span>🖤</span>
         </div>
         <form className="card-form">
           {/* need to add onClick={} to update counts */}
-          <input className="card-input" type="text" value="+1" />
+          <input onClick={increaseLikes} className="card-input" type="text" value="+1" />
         </form>
         <form className="card-form">
           <input className="card-input" type="text" value="Delete" />
@@ -28,4 +42,5 @@ const CardNote = () => {
     </section>
   );
 };
+
 export default CardNote;
