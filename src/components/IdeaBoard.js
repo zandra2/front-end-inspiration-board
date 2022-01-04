5ea3ebed1c55ebd3b56a0b9569451e27968d1c34
Zandra Nguyen <zandra.eng@gmail.com>
2022-01-04 12:02:27 -0800

    fixed console log bug and worked on axios post request. We still need to work on the 404 error.

--- src/components/IdeaBoard.js
+++ src/components/IdeaBoard.js
@@ -3,22 +3,25 @@ import "./IdeaBoard.css";
 import IdeaCard from "./IdeaCard";
 
 const IdeaBoard = (props) => {
-  console.log(props.board);
-  const { board, onDeleteCard, onAddLike, onDeleteBoard } = props;
+  // console.log(props.board);
+  const { board, onDeleteCard, onAddLike } = props; //removed onDeleteBoard
 
-  const deleteBoard = () => {
-    onDeleteBoard (board.board_id);
-  };
+  //TODO: **** Delete Board Button *************
+  // const deleteBoard = () => {
+  //   onDeleteBoard(board.board_id);
+  // };
 
+  //adding a ? at the end of the variable to tell React to wait and see what loaded before sending an error.
   return (
     <div>
-      <div className="card-board">Cards for {board.title}</div>
-      <div className="card-board">
+      <div className="card-board">Cards for {board?.title}</div>
+
+      {/* <div className="card-board">
         <button onClick={deleteBoard} className="delete-button" type="button">
           Delete Board
         </button>
-      </div>
-      {board.cards.map((card) => {
+      </div> */}
+      {board?.cards.map((card) => {
         return (
           <IdeaCard
             key={card.card_id}
